fe49e4e52db827369fe64e215a5f6f6e0c36e08a
Ivan Robles <irae45@gmail.com>
2026-02-22 01:46:41 -0600

    add github actions workflows for deploy, linter, and tests.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-oxc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+  }
 });
